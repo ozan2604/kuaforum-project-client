7176ee317773466795b7ba3e6ff33af73b3bba66
ozan2604 <ozansami04@gmail.com>
2026-07-26 22:10:03 +0300

    rez<z

--- src/components/Chatbot.tsx
+++ src/components/Chatbot.tsx
@@ -336,7 +336,7 @@ export const Chatbot: React.FC = () => {
                 loop
                 preload="auto"
                 playsInline
-                src="https://cdn.pixabay.com/download/audio/2022/05/27/audio_1808fbf07a.mp3?filename=lofi-study-112191.mp3"
+                src="https://cdn.pixabay.com/audio/2022/05/27/audio_1808fbf07a.mp3"
             />
 
             {/* Chatbot Window */}
